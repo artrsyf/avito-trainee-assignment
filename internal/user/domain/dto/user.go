ac84965e18fb7c1f915cf6b6ba00c0e0859f2eee
artrsyf <rom.fadeev2017@yandex.ru>
2025-02-11 21:47:14 +0300

    Create request-entity mapper

--- internal/user/domain/dto/user.go
+++ internal/user/domain/dto/user.go
@@ -1,9 +1,24 @@
 package dto
 
 import (
+	sessionDTO "github.com/artrsyf/avito-trainee-assignment/internal/session/domain/dto"
 	"github.com/artrsyf/avito-trainee-assignment/internal/user/domain/entity"
 	"github.com/artrsyf/avito-trainee-assignment/internal/user/domain/model"
+	"golang.org/x/crypto/bcrypt"
 )
+
+func AuthRequestToEntity(authRequest *sessionDTO.AuthRequest) (*entity.User, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(authRequest.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	return &entity.User{
+		Username:     authRequest.Username,
+		Coins:        1000, /*TODO magic number*/
+		PasswordHash: string(hashedPassword),
+	}, nil
+}
 
 func ModelToEntity(user *model.User) *entity.User {
 	return &entity.User{
