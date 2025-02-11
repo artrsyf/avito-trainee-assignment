4a36d25bf9d168d417cbec6225f94e1ea99a0e88
artrsyf <rom.fadeev2017@yandex.ru>
2025-02-12 01:40:14 +0300

    Fix negative cases handling

--- internal/session/usecase/usecase.go
+++ internal/session/usecase/usecase.go
@@ -1,13 +1,13 @@
 package usecase
 
 import (
-	"errors"
 	"time"
 
 	sessionDTO "github.com/artrsyf/avito-trainee-assignment/internal/session/domain/dto"
 	sessionEntity "github.com/artrsyf/avito-trainee-assignment/internal/session/domain/entity"
 	sessionRepo "github.com/artrsyf/avito-trainee-assignment/internal/session/repository"
 	userDTO "github.com/artrsyf/avito-trainee-assignment/internal/user/domain/dto"
+	userEntity "github.com/artrsyf/avito-trainee-assignment/internal/user/domain/entity"
 	userRepo "github.com/artrsyf/avito-trainee-assignment/internal/user/repository"
 )
 
@@ -32,13 +32,13 @@ func NewSessionUsecase(sessionRepository sessionRepo.SessionRepositoryI, userRep
 }
 
 func (uc *SessionUsecase) Signup(authRequest *sessionDTO.AuthRequest) (*sessionEntity.Session, error) {
-	ok, err := uc.userRepo.GetByUsername(authRequest.Username)
-	if err != nil {
-		return nil, err
+	_, err := uc.userRepo.GetByUsername(authRequest.Username)
+	if err == nil {
+		return nil, userEntity.ErrAlreadyCreated
 	}
 
-	if ok != nil {
-		return nil, errors.New("user is already signed up")
+	if err != userEntity.ErrIsNotExist {
+		return nil, err
 	}
 
 	userEntity, err := userDTO.AuthRequestToEntity(authRequest)
@@ -51,7 +51,7 @@ func (uc *SessionUsecase) Signup(authRequest *sessionDTO.AuthRequest) (*sessionE
 		return nil, err
 	}
 
-	sessionEntity, err := sessionDTO.AuthRequestToEntity(
+	session, err := sessionDTO.AuthRequestToEntity(
 		authRequest,
 		createdUser.ID,
 		time.Now().Add(15*time.Minute), /*TODO magic dates*/
@@ -61,12 +61,16 @@ func (uc *SessionUsecase) Signup(authRequest *sessionDTO.AuthRequest) (*sessionE
 		return nil, err
 	}
 
-	_, err = uc.sessionRepo.Check(sessionEntity.UserID)
-	if err != nil {
+	_, err = uc.sessionRepo.Check(session.UserID)
+	if err == nil {
+		return nil, sessionEntity.ErrAlreadyCreated
+	}
+
+	if err != sessionEntity.ErrNoSession {
 		return nil, err
 	}
 
-	createdSessionModel, err := uc.sessionRepo.Create(sessionEntity)
+	createdSessionModel, err := uc.sessionRepo.Create(session)
 	if err != nil {
 		return nil, err
 	}
@@ -75,42 +79,3 @@ func (uc *SessionUsecase) Signup(authRequest *sessionDTO.AuthRequest) (*sessionE
 
 	return createdSessionEntity, nil
 }
-
-/*TODO Delete method*/
-// func (uc *SessionUsecase) Create(authRequest *sessionDTO.SignupRequest) (*sessionEntity.Session, error) {
-// 	sessionEntity, err := sessionDTO.SignupRequestToEntity(
-// 		authRequest,
-
-// 		time.Now().Add(15*time.Minute), /*TODO*/
-// 		time.Now().Add(24*time.Hour),   /*TODO*/
-// 	)
-
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	_, err = uc.sessionRepo.Check(uint(sessionEntity.UserID))
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	createdSessionModel, err := uc.sessionRepo.Create(sessionEntity)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	createdSessionEntity := sessionDTO.SessionModelToEntity(createdSessionModel)
-
-// 	return createdSessionEntity, nil
-// }
-
-// func (uc *SessionUsecase) Check(userID uint) (*sessionEntity.Session, error) {
-// 	sessionModel, err := uc.sessionRepo.Check(userID)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	sessionEntity := sessionDTO.SessionModelToEntity(sessionModel)
-
-// 	return sessionEntity, nil
-// }
