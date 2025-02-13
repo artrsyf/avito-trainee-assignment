b9d615a6cadd917f356982199b34c60fb3be9b06
artrsyf <rom.fadeev2017@yandex.ru>
2025-02-13 22:39:14 +0300

    Fix init.sql script

--- scripts/sql/init.sql
+++ scripts/sql/init.sql
@@ -9,8 +9,8 @@ CREATE TABLE IF NOT EXISTS transactions (
     id SERIAL PRIMARY KEY,
     sender_user_id INTEGER NOT NULL,
     receiver_user_id INTEGER NOT NULL,
-    amount INT NOT NULL CHECK (amount > 0)
-    FOREIGN KEY (sender_user_id) REFERENCES users (id) ON DELETE SET NULL
+    amount INT NOT NULL CHECK (amount > 0),
+    FOREIGN KEY (sender_user_id) REFERENCES users (id) ON DELETE SET NULL,
     FOREIGN KEY (receiver_user_id) REFERENCES users (id) ON DELETE SET NULL
 );
 
