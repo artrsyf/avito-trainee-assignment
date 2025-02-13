877122060bd6edb05689ef0437a11ebff46f25a5
artrsyf <rom.fadeev2017@yandex.ru>
2025-02-13 15:52:18 +0300

    Update entrypoint

--- cmd/main.go
+++ cmd/main.go
@@ -13,13 +13,19 @@ import (
 	_ "github.com/lib/pq"
 
 	"github.com/artrsyf/avito-trainee-assignment/config"
+	"github.com/artrsyf/avito-trainee-assignment/middleware"
 
 	sessionRepository "github.com/artrsyf/avito-trainee-assignment/internal/session/repository/redis"
+	transactionRepository "github.com/artrsyf/avito-trainee-assignment/internal/transaction/repository/postgres"
 	userRepository "github.com/artrsyf/avito-trainee-assignment/internal/user/repository/postgres"
 
+	uow "github.com/artrsyf/avito-trainee-assignment/internal/user/uow/postgres"
+
 	sessionUsecase "github.com/artrsyf/avito-trainee-assignment/internal/session/usecase"
+	transactionUsecase "github.com/artrsyf/avito-trainee-assignment/internal/transaction/usecase"
 
 	sessionDelivery "github.com/artrsyf/avito-trainee-assignment/internal/session/delivery/http"
+	transactionDelivery "github.com/artrsyf/avito-trainee-assignment/internal/transaction/delivery/http"
 )
 
 func main() {
@@ -32,7 +38,6 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(cfg)
 
 	postgresDSN := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		os.Getenv("POSTGRES_HOST"),
@@ -71,17 +76,30 @@ func main() {
 
 	userRepo := userRepository.NewUserPostgresRepository(postgresConnect)
 	sessionRepo := sessionRepository.NewSessionRedisRepository(redisConn)
+	transactionRepo := transactionRepository.NewTransactionPostgresRepository(postgresConnect)
+
+	transactionUOW := uow.NewPostgresUnitOfWork(postgresConnect)
 
 	sessionUC := sessionUsecase.NewSessionUsecase(
 		sessionRepo,
 		userRepo,
 		cfg.User,
 	)
+	transactionUC := transactionUsecase.NewTransactionUsecase(
+		transactionRepo,
+		userRepo,
+		transactionUOW,
+	)
 
 	authHandler := sessionDelivery.NewSessionHandler(sessionUC)
+	transactionHandler := transactionDelivery.NewTransactionHandler(transactionUC)
 
 	router.Handle("/api/auth",
 		http.HandlerFunc(authHandler.Auth)).Methods("POST")
+
+	router.Handle("/api/sendCoin",
+		middleware.ValidateJWTToken(
+			http.HandlerFunc(transactionHandler.SendCoins))).Methods("POST")
 
 	fmt.Println("server starts on :8080")
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", 8080), router))
