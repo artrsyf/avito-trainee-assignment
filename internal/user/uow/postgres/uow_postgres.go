ea586c59916d8062148567ddc1a666f143f45cbb
artrsyf <rom.fadeev2017@yandex.ru>
2025-02-13 15:48:49 +0300

    Create user unit of work

---
+++ internal/user/uow/postgres/uow_postgres.go
@@ -0,0 +1,48 @@
+package postgres
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+type PostgresUnitOfWork struct {
+	db *sql.DB
+	tx *sql.Tx
+}
+
+func NewPostgresUnitOfWork(db *sql.DB) *PostgresUnitOfWork {
+	return &PostgresUnitOfWork{db: db}
+}
+
+func (u *PostgresUnitOfWork) Begin(ctx context.Context) error {
+	tx, err := u.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	u.tx = tx
+	return nil
+}
+
+func (u *PostgresUnitOfWork) Exec(query string, args ...any) (sql.Result, error) {
+	if u.tx == nil {
+		return nil, fmt.Errorf("transaction has not been started")
+	}
+	return u.tx.Exec(query, args...)
+}
+
+func (u *PostgresUnitOfWork) Commit() error {
+	if u.tx == nil {
+		return fmt.Errorf("transaction has not been started")
+	}
+
+	return u.tx.Commit()
+}
+
+func (u *PostgresUnitOfWork) Rollback() error {
+	if u.tx == nil {
+		return fmt.Errorf("transaction has not been started")
+	}
+
+	return u.tx.Rollback()
+}
---
+++ internal/user/uow/uow.go
@@ -0,0 +1,13 @@
+package uow
+
+import (
+	"context"
+	"database/sql"
+)
+
+type UnitOfWorkI interface {
+	Begin(ctx context.Context) error
+	Exec(query string, args ...any) (sql.Result, error)
+	Commit() error
+	Rollback() error
+}
