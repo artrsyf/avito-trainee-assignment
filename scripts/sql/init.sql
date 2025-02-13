7e7a71ac9a9028dec39dd85d56615494f61b6995
artrsyf <rom.fadeev2017@yandex.ru>
2025-02-13 15:51:34 +0300

    Update init.sql with transactions table

--- scripts/sql/init.sql
+++ scripts/sql/init.sql
@@ -4,3 +4,10 @@ CREATE TABLE IF NOT EXISTS users (
     coins INTEGER NOT NULL DEFAULT 0 CHECK (coins >= 0),
     password_hash TEXT NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS transactions (
+    id SERIAL PRIMARY KEY,
+    sender_username VARCHAR(255) NOT NULL,
+    receiver_username VARCHAR(255) NOT NULL,
+    amount INT NOT NULL CHECK (amount > 0)
+);
